07c822615edad26e914a0e38d7260b5979576b01
Max Natanael Starke <maxnatanaelstk@gmail.com>
2023-06-24 16:52:18 -0300

    saving user

--- core/api/serializers/user_serializer.py
+++ core/api/serializers/user_serializer.py
@@ -1,7 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from core.models import User
 
+
 class UserSerializer(ModelSerializer):
+    password = CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password', 'birth_date')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
