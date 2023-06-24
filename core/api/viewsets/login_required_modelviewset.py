8a4dee1f5957e1dc5116520c27bc4fd598ca5790
Max Natanael Starke <maxnatanaelstk@gmail.com>
2023-06-24 16:43:29 -0300

    filtragem dinamica

--- core/api/viewsets/login_required_modelviewset.py
+++ core/api/viewsets/login_required_modelviewset.py
@@ -1,10 +1,26 @@
 import json
 from rest_framework import authentication, permissions
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
+class CustomSearchFilter(filters.SearchFilter):
+
+    def get_search_fields(self, view, request):
+        # self.search_param = 'icontains'
+        related_model = view.queryset.model._meta.get_fields()                          
+        new_fields = [f'{field.attname}' for field in related_model if (
+            field.concrete and field.attname != 'id' and field.__class__.__name__ != 'ForeignKey') 
+        ]
+        return new_fields
+
+    
+    
 class LoginRequiredModelViewSet(ModelViewSet):
     authentication_classes = (authentication.SessionAuthentication,authentication.TokenAuthentication)
     permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = [CustomSearchFilter, DjangoFilterBackend]
 
-    filterset_fields = '__all__'
+    search_fields = '__all__'
+    ordering_fields = '__all__'
--- foodtips/settings.py
+++ foodtips/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 INSTALLED_APPS = [
